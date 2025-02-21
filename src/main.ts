5bdfe8805f61f1560ca9d8adf5fd41d77f698062
emsmoraes <eduardomoraesdeveloper@gmail.com>
2025-02-21 18:32:48 -0300

    chore: upgrade project dependencies and restructure application

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,34 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+import { EnvService } from './modules/env/env.service';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create(AppModule, { cors: true });
+
+  const envService = app.get(EnvService);
+
+  const port = envService.get('PORT');
+
+  const config = new DocumentBuilder()
+    .setTitle('Mamba Campaigns api docs')
+    .setDescription('Api para o projeto Mamba Campaigns')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  await app.listen(port);
 }
 bootstrap();
